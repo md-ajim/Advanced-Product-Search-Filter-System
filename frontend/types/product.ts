93b16a69b05544a0d25c0ca293fbc64aa1f83ffa
MD AJIM <86977364+md-ajim@users.noreply.github.com>
2025-10-02 10:23:52 +0600

    Define Category interfaces in product.
    Added Type interfaces for `Product` and `` to structure and ensure consistency handling product. This includes fields for product details, optional attributes, and category enumeration.

---
+++ frontend/types/product.ts
@@ -0,0 +1,20 @@
+export interface Category {
+    id: number;
+    name: "ALL" | "CLOTHES" | "HOME" | "ELECTRONICS" | "BEAUTY" | "SPORTS";
+    title?: string | null;
+}
+
+export interface Product {
+    id: number;
+    name: string;
+    title?: string | null;
+    category?: Category | null;
+    description: string;
+    price: string; // Decimal as string to preserve precision
+    size?: string | null;
+    color?: string | null;
+    image?: string | null;
+    is_active: boolean;
+    created_at: string; // ISO date string
+    updated_at: string; // ISO date string
+}
