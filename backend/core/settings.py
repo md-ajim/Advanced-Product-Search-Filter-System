c8b8b3ce194d35c9becac918dc490b8bfeab4d9a
MD AJIM <86977364+md-ajim@users.noreply.github.com>
2025-10-05 22:55:13 +0600

    Ref static and media file settings.
    ated Django static and media file configuration for better clarity and maintainability. Updated Next domain to include the correct/images path for images

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -153,16 +153,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-MEDIA_URL='/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-
-STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-MEDIA_ROOT = os.path.join( BASE_DIR , 'mediafiles')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Media files (user-uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["https://advanced-product-search-filter-system.onrender.com"],
+    domains: ["https://advanced-product-search-filter-system.onrender.com/media/images/"],
   },
 };
 
